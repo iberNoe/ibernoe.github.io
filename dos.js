0f622797f9010e1ee439e2823c779fe7eef10440
noe aquino <97256676+iberNoe@users.noreply.github.com>
2023-09-24 22:18:40 +0000

    Update dos.js

--- dos.js
+++ dos.js
@@ -1,24 +1,24 @@
-unction main() {
+function main() {
   // Suppress in ReplView
-  if (window.location.hostname.split('.')[1] === 'id') return;
+  if (window.location.hostname.split(".")[1] === "id") return;
 
   // Theme is passed from the script tag, default blue
   const themes = {
-    dark: { fg: '#F5F9FC', bg: '#130F26' },
-    light: { fg: '#130F26', bg: '#F5F9FC' },
-    red: { fg: '#F5F9FC', bg: '#FA4B4B' },
-    orange: { fg: '#F5F9FC', bg: '#D96D00' },
-    yellow: { fg: '#F5F9FC', bg: '#A68A00' },
-    lime: { fg: '#F5F9FC', bg: '#639400' },
-    green: { fg: '#F5F9FC', bg: '#00A11B' },
-    teal: { fg: '#F5F9FC', bg: '#0093B0' },
-    blue: { fg: '#F5F9FC', bg: '#0F87FF' },
-    blurple: { fg: '#F5F9FC', bg: '#8E78FF' },
-    purple: { fg: '#F5F9FC', bg: '#B266FF' },
-    magenta: { fg: '#F5F9FC', bg: '#EB3BEB' },
-    pink: { fg: '#F5F9FC', bg: '#F545BA' },
+    dark: { fg: "#F5F9FC", bg: "#130F26" },
+    light: { fg: "#130F26", bg: "#F5F9FC" },
+    red: { fg: "#F5F9FC", bg: "#FA4B4B" },
+    orange: { fg: "#F5F9FC", bg: "#D96D00" },
+    yellow: { fg: "#F5F9FC", bg: "#A68A00" },
+    lime: { fg: "#F5F9FC", bg: "#639400" },
+    green: { fg: "#F5F9FC", bg: "#00A11B" },
+    teal: { fg: "#F5F9FC", bg: "#0093B0" },
+    blue: { fg: "#F5F9FC", bg: "#0F87FF" },
+    blurple: { fg: "#F5F9FC", bg: "#8E78FF" },
+    purple: { fg: "#F5F9FC", bg: "#B266FF" },
+    magenta: { fg: "#F5F9FC", bg: "#EB3BEB" },
+    pink: { fg: "#F5F9FC", bg: "#F545BA" },
   };
-  const color = themes[document.currentScript.getAttribute('theme') || 'blue'];
+  const color = themes[document.currentScript.getAttribute("theme") || "blue"];
 
   const badge = `
   <style>
@@ -72,7 +72,7 @@ unction main() {
   </a>
   `;
 
-  document.body.insertAdjacentHTML('beforeend', badge);
+  document.body.insertAdjacentHTML("beforeend", badge);
 }
 
 main();
